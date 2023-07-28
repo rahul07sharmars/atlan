06505841f9e17bbd35cc9ff4949e29edf91e7980
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2023-07-28 20:22:59 +0530

    init

---
+++ src/main/java/com/example/atlan/AtlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.atlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atlan/AtlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
