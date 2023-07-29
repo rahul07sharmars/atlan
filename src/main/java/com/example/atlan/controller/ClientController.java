5965f6a8efa97a99555b83123bd80f5df407c878
rahul07sharmars <58891614+rahul07sharmars@users.noreply.github.com>
2023-07-29 14:35:45 +0530

    download excel working

--- src/main/java/com/example/atlan/controller/ClientController.java
+++ src/main/java/com/example/atlan/controller/ClientController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
-
 import com.example.atlan.entity.ClientDetails;
 import com.example.atlan.service.ClientService;
 
--- src/main/java/com/example/atlan/controller/TranslateController.java
+++ src/main/java/com/example/atlan/controller/TranslateController.java
@@ -1,15 +1,29 @@
 package com.example.atlan.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
 import com.google.cloud.translate.Translation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 @RestController
 public class TranslateController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(TranslateController.class);
+	
 	private static final String API_KEY = "AIzaSyCxebiUD2XyTAXAJrOwK5oYNqXq-_7_PpU";
 
-    public static String translateText(String sourceLanguage, String targetLanguage, String text) {
+	@GetMapping("/translate/{lang1}{lang2}{text}")
+//	@PathVariable("id") Integer clientId
+//	   String sourceLanguage, String targetLanguage, String text
+    public static String translateText(@PathVariable("lang1") String sourceLanguage, @PathVariable("lang2") String targetLanguage, 
+    		@PathVariable("text") String text ) {
+		logger.info("inside translateText function of TranslateController");
         Translate translate = TranslateOptions.newBuilder().setApiKey(API_KEY).build().getService();
         
         Translation translation = translate.translate(text, Translate.TranslateOption.sourceLanguage(sourceLanguage),
